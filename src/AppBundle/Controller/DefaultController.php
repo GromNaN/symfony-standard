c69f10021fc22d6e35e5b826c0df9b4f28038513
Jérôme TAMARELLE <jtamarel@prismamedia.com>
2016-05-21 16:56:21 +0200

    Update to show optionnal URL part without redirection.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * Route("/test/", name="homepage")
+     * @Route("/test/{slug}", requirements={"slug" = "[a-z]*"}, name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $slug = '')
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return new Response(sprintf('The slug is: "%s"', $slug));
     }
 }
